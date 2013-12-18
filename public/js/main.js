84e4eaa53bc63a6df4ddca3bf59491e27f17265c
Domenico Leone Luciani <domenicoleoneluciani@gmail.com>
2013-12-18 20:18:05 +0100

    removed javascript useless

--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-$('body').scrollspy({ target: '.navbar' });
-
-$(".navbar ul li a[href^='#']").on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 300);
-});
